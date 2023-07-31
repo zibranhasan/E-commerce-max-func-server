7cc29693c61148d9adef5c162b5f3836b31b870b
Zibran_Hasan <100549342+zibranhasan@users.noreply.github.com>
2023-07-31 20:12:18 +0600

    Your commit message

---
+++ server.js
@@ -0,0 +1,65 @@
+import express from "express";
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from "./config/db.js";
+import authRoutes from './routes/authRoute.js';
+import categoryRoutes from './routes/categoryRoutes.js';
+import productRoutes from "./routes/productRoutes.js"
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+//configure env
+dotenv.config();//root e ace; jdi onno kthao thakto({path:''})
+
+
+
+//database connect
+connectDB()
+
+
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//rest object
+const app = express();
+
+//middlewares
+app.use(cors());
+app.use(express.json())
+app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, '../client/build')))
+
+
+
+//routes
+app.use("/api/v1/auth", authRoutes)
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/product", productRoutes);
+
+
+
+
+
+//rest api
+app.get("/", (req, res) => {
+    res.send("<h1>Welcome to ecommerce app</h1>"
+    )
+});
+
+// app.use('*', function (req, res) {
+//     res.sendFile(path.join(__dirname, '..client/build/index.html'));
+// })
+
+
+
+//PORT
+const PORT = process.env.PORT || 8080;
+
+
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server Running on ${PORT}`.bgCyan.white);
+})
